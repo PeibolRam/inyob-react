d25443d8eb56cf73a0247edaedc7287e7f562eb4
PabloMeibi <pramirez@meibi.mx>
2023-12-30 16:37:02 -0600

    add style esmpresas

--- src/pages/Empresas.jsx
+++ src/pages/Empresas.jsx
@@ -1,20 +1,23 @@
 import HubspotForm from 'react-hubspot-form'
 
+import '../styles/Empresas.css'
+
 export const Empresas = () => {
   return (
     <>
-      <section className='content'>
-        <div className='title-section'>
-          <h2>Dé el salto hacia una estrategia de reclutamiento más inteligente, eficiente y rentable. Conviértase en líder de su industria al contar con el mejor talento, de la mano de inyob. ¡Hablemos!</h2>
-
+      <section className='empresas-content'>
+        <div className='content'>
+          <div className='title-section'>
+            <h2 style={{ color: '#fff' }}>Dé el salto hacia una estrategia de reclutamiento más inteligente, eficiente y rentable. Conviértase en líder de su industria al contar con el mejor talento, de la mano de inyob. ¡Hablemos!</h2>
+          </div>
+          <HubspotForm
+            portalId='44645563'
+            formId='affe38d1-22a2-4836-a91f-c3f3d156dfc3'
+            onSubmit={() => console.log('Submit!')}
+            onReady={(form) => console.log(form + 'Form ready!')}
+            loading={<div className='loader-container'><span className="loader"></span></div>}
+          />
         </div>
-        <HubspotForm
-          portalId='44645563'
-          formId='affe38d1-22a2-4836-a91f-c3f3d156dfc3'
-          onSubmit={() => console.log('Submit!')}
-          onReady={(form) => console.log(form + 'Form ready!')}
-          loading={<div className='loader-container'><span className="loader"></span></div>}
-        />
       </section>
     </>
   )
