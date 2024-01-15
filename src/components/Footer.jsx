3540de4be1588c7efc4f3cb0cb4486ed3d5bfe95
PabloMeibi <pramirez@meibi.mx>
2024-01-14 19:29:10 -0600

    nuevo logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import Logo from '../assets/images/logoinyob.svg'
+import Logo from '../assets/images/inyobslogan.png'
 import { Link } from 'react-router-dom'
 import { scrollToTop } from '../utils/utils'
 
