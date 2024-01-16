55933ffd28f6ac24f57b0cd1018d8f38346ce8ec
PabloMeibi <pramirez@meibi.mx>
2024-01-16 15:06:40 -0600

    meta

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import { About } from './pages/About.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Hablemos } from './pages/Hablemos.jsx'
 import { Empresas } from './pages/Empresas.jsx'
+import { Helmet } from 'react-helmet';
+import LogoSlogan from './assets/images/inyobslogan.png'
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta property="og:title" content="Inyob" />
+        <meta property="og:description" content="Encuentra tu empleo ideal" />
+        <meta property="og:image" content={LogoSlogan} />
+      </Helmet>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
